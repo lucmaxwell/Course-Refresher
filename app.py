413b26a90e6c1cc69a47bec607ffc52f38c9babb
lucmaxwell <lucmaxwell8@gmail.com>
2021-05-06 21:42:48 -0500

    Wrote the code again to make it cleaner

--- app.py
+++ app.py
@@ -1,87 +1,63 @@
-import time
+#Import statements
+import time 
 from selenium import webdriver
-import binascii
-import urllib.request
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-logInTime = 30
-refreshTime = 0
-timeBetweenFirstTwoScreenshots = 3.7 #Not recommended to go lower than 2 since page will not be loaded before screenshots are taken
-timeBetweenClicks = 0.5
-Space = ['/html/body/form/div[2]/div[4]/div[2]/div/div/div/div/div/div/div[3]/div[4]/div/div[1]/div/div[1]/div[1]/div[2]/div/div/table/tbody/tr[1]/td[5]/div/div/span'
-         ]
-
+#Inputs
 btnToClick = [
     '/html/body/form/div[2]/div[4]/div[2]/div/div/div/div/div/div/div[3]/div[6]/div/div/div/div[3]/div/div[2]/div/div/table/tbody/tr[1]/td[1]/div/div/div/input',
-    '/html/body/form/div[2]/div[4]/div[2]/div/div/div/div/div/div/div[3]/div[6]/div/div/div/div[3]/div/div[2]/div/div/table/tbody/tr[3]/td[1]/div/div/div/input',
     '/html/body/form/div[2]/div[4]/div[2]/div/div/div/div/div/div/div[3]/div[6]/div/div/div/div[4]/div[1]/span/a',
     '/html/body/form/div[1]/div[3]/div[2]/div/div[2]/div/div[2]/div[1]/span/a/span',
-    ]
+]
+
+eleToCheck = [
+    '/html/body/form/div[2]/div[4]/div[2]/div/div/div/div/div/div/div[3]/div[4]/div/div[1]/div/div[1]/div[1]/div[2]/div/div/table/tbody/tr[1]/td[2]/div/div/span',
+    '/html/body/form/div[2]/div[4]/div[2]/div/div/div/div/div/div/div[3]/div[4]/div/div[1]/div/div[1]/div[1]/div[2]/div/div/table/tbody/tr[3]/td[2]/div/div/span',
+    '/html/body/form/div[2]/div[4]/div[2]/div/div/div/div/div/div/div[3]/div[4]/div/div[1]/div/div[1]/div[1]/div[2]/div/div/table/tbody/tr[5]/td[2]/div/div/span',
+]
+
+initialState = 'Full'.lower() #Change to get input later
+
+logInTime = 25
+timeBetweenClicks = 0.5
 
 def start():
-    global iterable
-    global photos
-    global counter
-    counter = 0
-    iterable = True
-    photos = ['0', '0']
+    browser = webdriver.Chrome()
+    browser.get('https://www.beartracks.ualberta.ca/')
+    time.sleep(logInTime)
+    return browser
+
+def loadPage():
     browser.refresh()
-    time.sleep(timeBetweenFirstTwoScreenshots)
-    url = browser.current_url
-    urllib.request.urlretrieve(url, "test{int(iterable)}.txt")
-    #browser.save_screenshot(f"screenshot{int(iterable)}.png")
-    #with open(f"screenshot{int(iterable)}.png", 'rb') as image:
-    #    hexa = binascii.hexlify(image.read())
-    #photos[int(iterable)] = hexa
-    iterable = not iterable
+    wait = WebDriverWait(browser, 20,0.5)
+    element = wait.until(EC.presence_of_element_located((By.XPATH, eleToCheck[0])))   
+
+def getText():
+    eleText = [browser.find_element_by_xpath(eleToCheck[i]).text.lower() for i in range(len(eleToCheck))]
+    return eleText
+
+def comparison():
+    if eleText == [initialState for i in range(len(eleToCheck))]:
+        print("All classes are full, refreshing to check again.")
+        loadPage()
+        getText()
+        comparison()
+    else:
+        print("Changes detected. Clicking buttons set to be clicked. ")
+        for i in btnToClick:
+            time.sleep(timeBetweenClicks)
+            button = browser.find_element_by_xpath(i)
+            ActionChains(browser).move_to_element(button).click(button).perform()
+        time.sleep(5)
 
 
-browser = webdriver.Chrome()
-browser.get('https://www.beartracks.ualberta.ca/')
-
-time.sleep(logInTime)
-
-start()
-
-while True:
-    try:
-        browser.refresh()
-        wait = WebDriverWait(browser, 20,0.5)
-        element =wait.until(EC.presence_of_element_located((By.XPATH,'/html/body/form/div[2]/div[4]/div[2]/div/div/div/div/div/div/div[3]/div[6]/div/div/div/div[3]/div/div[2]/div/div/table/tbody/tr[1]/td[1]/div/div/div/input')))   
-        url = browser.current_url
-        urllib.request.urlretrieve(url, "test{int(iterable)}.txt")
-        #browser.save_screenshot(f"screenshot{int(iterable)}.png")
-        #with open(f"screenshot{int(iterable)}.png", 'rb') as image:
-        #    hexa = binascii.hexlify(image.read())
-        #photos[int(iterable)] = hexa
-        
-        iterable = not iterable
-        counter += 1
-
-        if photos[1] == photos [0] :
-            #driver.find_element(By.XPATH, '/html/body/form/div[2]/div[4]/div[2]/div/div/div/div/div/div/div[3]/div[4]/div/div[1]/div/div[1]/div[1]/div[2]/div/div/table/tbody/tr[1]/td[2]/div/div/span')!=0
-            print(f"No changes detected. Searching again for changes. ({counter})")
-            time.sleep(refreshTime)
-            raise Exception("No change")
-
-        else:
-            print("Changes detected. Clicking buttons set to be clicked. ")
-            for i in btnToClick:
-                time.sleep(timeBetweenClicks)
-                button = browser.find_element_by_xpath(i)
-                ActionChains(browser).move_to_element(button).click(button).perform()
-            time.sleep(10)
-            message = input("Type 'exit' and then press enter to exit the program. Press any other key(s) and press enter to continue running the program. ")
-            if message.strip() == 'exit' or message.strip() == 'Exit':
-                break
-            else:
-                start()
-    except:
-        pass
-
-
+browser = start()
+loadPage()
+eleText = getText()
+comparison()
 browser.close()
+print('done')
